6698154e0aa54cf5772ad2d0b8f19fe9a3ff71c5
spinner5 <166389859+antzax@users.noreply.github.com>
2026-02-28 13:20:41 +0200

    Add query for items

---
+++ src/db/queries.js
@@ -0,0 +1,8 @@
+const pool = require("./pool");
+
+async function getAllItems() {
+  const { rows } = await pool.query("SELECT * FROM items");
+  return rows;
+}
+
+module.exports = { getAllItems };
