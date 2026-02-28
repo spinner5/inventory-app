c06168d2cfe24d34777ddd97750cf9323ecc6797
spinner5 <166389859+antzax@users.noreply.github.com>
2026-02-28 12:50:39 +0200

    Setup express

---
+++ src/app.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+
+app.get("/health", (req, res) => {
+  res.send("ok");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`App listening port ${PORT}`);
+});
